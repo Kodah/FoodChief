ab2300aa83772fa3ac5e159e71cd603e3c7bd27e
Kodah <tom_1440@hotmail.co.uk>
2016-02-01 13:48:42 +0000

    Implement getAuthenticated in /authenticate post call. Add JWT

--- routes/authentication.js
+++ routes/authentication.js
@@ -12,9 +12,22 @@ router.get('/', function(req, res, next) {
     res.json('Authentication Api');
 });
 
-router.post('/authenticate', function(req, res, next) {
+router.post('/', function(req, res, next) {
     
+    var user = new User();
+    username = req.body.username;
+    password = req.body.password;
 
+    User.getAuthenticated(username, password, function(err, user, reason) {
+        if (!err) {
+            res.send("Login successful").status(200);
+        }
+        else
+        {
+            res.status(reason).send(err); 
+        }
+
+        });
 });
  
 
